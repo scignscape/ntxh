f37db6f43e0cdf4ed92ae7ca458e929d18e87992
ScignScape <mereosemantics@gmail.com>
2021-04-18 23:22:53 -0400

    update util

--- dev/consoles/gtagml/sdi/template/run-pdflatex.sh
+++ dev/consoles/gtagml/sdi/template/run-pdflatex.sh
@@ -1,4 +1,5 @@
 pdfl=`cat %R%/../preferred/preferred-latex`
+pdfl=${pdfl/\~/$HOME/}
 echo $pdfl
 TEXINPUTS=%I%:%U%/figures:%U%/extra:: $pdfl -jobname=%J% -synctex=1 -interaction=nonstopmode --shell-escape -output-dir=../../out  %F%
 
